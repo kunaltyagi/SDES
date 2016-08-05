2cd29dab7cd8dd53e17600713f77d8c533fbab1f
Kunal Tyagi <tyagi.kunal@live.com>
2016-08-05 07:34:12 +0530

    Adding files for solution to Exercise 1
    Added sample files, shell script

---
+++ assgn/bash/excercise1.sh
@@ -0,0 +1,44 @@
+#! /usr/env/bash
+
+FILE1=${1}
+FILE2=${2}
+output=${3:-final-output.csv}
+
+sum_data=
+
+# append partB marks and total marks to each line of partA
+while read line;
+do
+    rollnum=`echo ${line} | cut -d, -f1`
+    marks_a=`echo ${line} | cut -d, -f2`
+    if [ `echo ${line} | grep -c 'RollNum'` -eq 1 ]; then
+        line='RollNum, part A, Name, part B, Total, IsTied'
+        sum_data=${line}
+        continue
+    fi
+    if [ `echo ${line} | cut -d, -f2` -gt 20 ]; then
+        line=${line}', '`grep ${rollnum} ${FILE2} | cut -d, -f2`
+        # calculate sum
+        marks_b=`echo ${line} | cut -d, -f4`
+        line=${line}', '$((marks_a + marks_b))
+    else
+        line=${line}', NC, '${marks_a}
+    fi
+    sum_data=${sum_data}$'\n'${line}
+done < ${FILE1}
+
+# sort the non-header lines and find ties and non-tied scores
+sorted=`echo "${sum_data}" | tail -n+2 | sort -n -k5`
+no_tie=`echo "${sorted}" | uniq -f4 -u | sed 's/$/, No/g'`
+tie=`echo "${sorted}" | uniq -f4 -D | sed 's/$/, Yes/g'`
+
+# sort based on roll number
+sorted=${no_tie}$'\n'${tie}
+sorted=`echo "${sorted}" | sort -n -k1`
+
+# append sorted data to header line
+output_data=`echo "${sum_data}" | head -n1`
+output_data=${output_data}$'\n'${sorted}
+
+# reorder the columns, beautify them for final output
+echo "${output_data}" | awk -F, '{print $1", "$3", "$2", "$4", "$5", "$6}' | tr -s ' ' > ${output}
